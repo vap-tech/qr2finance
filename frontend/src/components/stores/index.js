c0da109319e3501c0ac8f8ff7daf20d294b6257f
v.petrenko <bafomet2016vitt@gmail.com>
2026-01-25 00:21:54 +0300

    Create index.js

---
+++ frontend/src/components/stores/index.js
@@ -0,0 +1,6 @@
+import Stores from "./Stores";
+export default Stores;
+
+export { default as StoreStatsTable } from "./StoreStatsTable";
+export { default as StoreCard } from "./StoreCard";
+export { default as StoreModal } from "./StoreModal";
