056fc50b5ce5397d94a5afb886e66ba936dc3a6b
v.petrenko <bafomet2016vitt@gmail.com>
2026-02-07 11:09:02 +0300

    Update main.py

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import cashiers, items, login, private, users, utils
+from app.api.routes import cashiers, items, login, private, shop, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(cashiers.router)
+api_router.include_router(shop.router)
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
